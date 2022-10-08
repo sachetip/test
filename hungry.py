cbbc44867cb7b2c483bb03256c2e6bf1034833b2
dev global <piyushsacheti@gmail.com>
2022-10-08 11:39:07 +0530

    updated hungry program with input

--- hungry.py
+++ hungry.py
@@ -1,1 +1,10 @@
-print ('I am hungry')
+print ("my  hungry program..........")
+hungry=input("Are you hungry?")
+if hungry=="yes":
+    print ("Eat Samosa")
+else:
+    print ("do your homework")
+
+
+
+
